dbc07a8d9b96e3a85de498720cb135c97e048daa
FilipeHSAraujo <filipehsaraujo@gmail.com>
2026-04-05 00:12:23 -0300

    docs: adicionando comentários detalhados sobre POO

--- src/C5/Java/Main.java
+++ src/C5/Java/Main.java
@@ -1,47 +1,71 @@
 package C5.Java;
 
-// 1. Classe base para todos os Ninjas
+/**
+ * 1. CLASSE ABSTRATA (CONCEITO DE HERANÇA)
+ * Usamos 'abstract' para que ninguém possa criar um "Ninja" genérico.
+ * Um ninja tem de pertencer a um clã específico.
+ */
 abstract class Ninja {
+    // Atributo protegido: as subclasses (Uzumaki, Uchiha) podem aceder diretamente
     protected String nome;
 
+    // CONSTRUTOR: Garante que o ninja já nasce com um nome
     public Ninja(String nome) {
         this.nome = nome;
     }
 
-    // Método abstrato: cada clã DEVE implementar o seu
+    /**
+     * MÉTODO ABSTRATO (POLIMORFISMO)
+     * Não definimos o que este método faz aqui.
+     * Cada clã "promete" que vai implementar a sua própria versão.
+     */
     public abstract void habilidadeEspecial();
 }
 
-// 2. Especialização do Clã Uzumaki
+/**
+ * 2. SUBCLASSE UZUMAKI (HERANÇA)
+ * 'extends' significa que Uzumaki herda tudo o que um Ninja tem.
+ */
 class Uzumaki extends Ninja {
+    
     public Uzumaki(String nome) {
-        super(nome);
+        super(nome); // Chama o construtor da classe pai (Ninja)
     }
 
+    // @Override: Indica que estamos a sobrescrever o comportamento genérico
     @Override
     public void habilidadeEspecial() {
-        System.out.println(nome + " usou: Rasengan!");
+        System.out.println(nome + " usou o Rasengan! (Grande quantidade de Chakra)");
     }
 }
 
-// 3. Especialização do Clã Uchiha
+/**
+ * 3. SUBCLASSE UCHIHA
+ */
 class Uchiha extends Ninja {
+    
     public Uchiha(String nome) {
         super(nome);
     }
 
     @Override
     public void habilidadeEspecial() {
-        System.out.println(nome + " usou: Chidori!");
+        System.out.println(nome + " usou o Chidori! (Ataque de Relâmpago)");
     }
 }
 
 public class Main {
+    /**
+     * O método main deve ser sempre 'public static void'
+     */
     public static void main(String[] args) {
-        // Uso de Polimorfismo: tratamos todos como "Ninja"
+
+        // Instanciando objetos usando Polimorfismo
+        // Podemos declarar como 'Ninja' (tipo genérico) mas criar como 'Uzumaki' (tipo específico)
         Ninja naruto = new Uzumaki("Naruto Uzumaki");
         Ninja sasuke = new Uchiha("Sasuke Uchiha");
 
+        // Executando os métodos
         naruto.habilidadeEspecial();
         sasuke.habilidadeEspecial();
     }
